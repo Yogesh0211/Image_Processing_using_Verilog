8206fa3d182461c40f1b184939a2cc5f357d599c
Yogesh Yadav <73029620+Yogesh0211@users.noreply.github.com>
2022-05-30 13:56:12 +0530

    Add files via upload

---
+++ parameter.v
@@ -0,0 +1,9 @@
+`define INPUTFILENAME		 "input.hex" // Input file name
+`define OUTPUTFILENAME		 "output.bmp"		// Output file name
+
+// Choose the operation of code 
+
+//`define BRIGHTNESS_OPERATION
+//`define INVERT_OPERATION
+//`define BLACKandWHITE_OPERATION
+`define THRESHOLD_OPERATION 
